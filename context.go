88e41cd87791bcacc4767e0544e235a7c32996d6
Neil E. Pearson <neil@pearson.sydney>
2024-01-29 14:39:38 +1100

    Add some missing doc comments

--- context.go
+++ context.go
@@ -8,6 +8,7 @@ type Context struct {
 	ctx context.Context
 }
 
+// Ctx is the context.Context with which the process's Job was scheduled.
 func (c *Context) Ctx() context.Context {
 	if c.ctx == nil {
 		return context.Background()
@@ -15,6 +16,8 @@ func (c *Context) Ctx() context.Context {
 	return c.ctx
 }
 
+// Progress reports the process's process. The given payload is passed to the Delegate.JobProgressed function of
+// delegates in the receiver and its ancestors.
 func (c *Context) Progress(payload any) {
 	// TODO: interrupt scheduler?
 	c.notify(func(d Delegate) { d.JobProgressed(c.process, payload) })
